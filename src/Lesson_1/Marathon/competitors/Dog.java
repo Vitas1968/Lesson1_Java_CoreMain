b0b9bf5bfe1971942db7727c8a793abd20b22271
Vitas1968 <dor1968@mail.ru>
2019-07-15 19:37:31 +0700

    перенес участников в отдельный пакет competitors

---
+++ src/Lesson_1/Marathon/competitors/Dog.java
@@ -0,0 +1,10 @@
+package Lesson_1.Marathon.competitors;
+
+import Lesson_1.Marathon.competitors.Animal;
+
+public class Dog extends Animal
+{
+    public Dog(String name) {
+        super("Пес", name, 500, 5, 20);
+    }
+}
