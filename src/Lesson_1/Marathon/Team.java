74ebfaab018afcb6558e560cc391dd231c4444b4
Vitas1968 <dor1968@mail.ru>
2019-07-15 22:11:02 +0700

    создан и реализован класс Team

--- src/Lesson_1/Marathon/Team.java
+++ src/Lesson_1/Marathon/Team.java
@@ -1,5 +1,32 @@
 package Lesson_1.Marathon;
 
+import Lesson_1.Marathon.competitors.Competitor;
+
 public class Team
 {
+    private String name;
+    private Competitor [] competitors;
+
+    public Team(String name, Competitor... competitors)
+    {
+        this.name = name;
+        this.competitors = competitors;
+    }
+
+    void infoAllCompetitors()
+    {
+        System.out.println("Team "+ name+":");
+        for (Competitor c : competitors) {
+            c.info();
+        }
+
+    }
+
+    void showResults()
+{
+    for (Competitor c : competitors) {
+        if (c.isOnDistance()) c.info();
+    }
+
+}
 }
