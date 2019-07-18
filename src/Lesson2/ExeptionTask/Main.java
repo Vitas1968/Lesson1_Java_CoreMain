89d5a8397936e8dff3182328908b09c21abe1846
Vitas1968 <dor1968@mail.ru>
2019-07-18 18:23:34 +0700

    создан метод main() и два массива

--- src/Lesson2/ExeptionTask/Main.java
+++ src/Lesson2/ExeptionTask/Main.java
@@ -2,4 +2,18 @@ package Lesson2.ExeptionTask;
 
 public class Main
 {
+    static String [][]array3x3 =
+            {       {"1","2","3"},
+                    {"4","5","6"},
+                    {"7","8","9"}};
+    static String [][]array4x4 =
+            {       {"1","2","3","4"},
+                    {"5","6","7","8"},
+                    {"9","10","11","12"},
+                    {"13","14","15","16"}};
+    
+    public static void main(String[] args)
+    {
+
+    }
 }
