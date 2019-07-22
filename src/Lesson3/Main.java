70cd8286165b2aa6868faed8c0e3cb673b072c74
Vitas1968 <dor1968@mail.ru>
2019-07-22 21:52:09 +0700

    добавлено тестирование класса TormentedArray

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -1,14 +1,14 @@
 package Lesson3;
 
-import jdk.nashorn.api.tree.ArrayLiteralTree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        // тест класса PhoneBook
+       /*
         PhoneBook phoneBook=new PhoneBook();
         phoneBook.add("Bill","89039872345");
         phoneBook.add("Stiv","8902456997","89039872345");
@@ -22,11 +22,17 @@ public class Main
         System.out.println(phoneBook.get("Bob"));
         System.out.println(phoneBook.get("Arni"));
         System.out.println(phoneBook.get("Lucy"));
+        */
+
+       // Тест класса TormentedArray
 
 
-
-
-
-
+        // Конструктор с аргументами
+       // TormentedArray array =new TormentedArray("Коля","Аня","Вася",
+        // "Коля","Аня","Женя");
+        // конструктор по умолчанию
+        TormentedArray array =new TormentedArray();
+        array.uniqValues();
+        array.duplicate();
     }
 }
