70ca93b648ee2237c599856520ea03080b37ed88
Vitas1968 <dor1968@mail.ru>
2019-07-22 18:22:30 +0700

    тест класса PhoneBook

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -1,5 +1,32 @@
 package Lesson3;
 
+import jdk.nashorn.api.tree.ArrayLiteralTree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        PhoneBook phoneBook=new PhoneBook();
+        phoneBook.add("Bill","89039872345");
+        phoneBook.add("Stiv","8902456997","89039872345");
+        phoneBook.add("Kate","89039872345","8902456997","8902456997");
+        phoneBook.add("Bob","89039872345","89039872345","89039872345","89039872345");
+        phoneBook.add("Arni","89039872345","89039872345","89039872345","89039872345","89039872345");
+        phoneBook.add("Lucy","89039872345","89039872345","89039872345","89039872345","89039872345","89039872345");
+
+
+        System.out.println(phoneBook.get("Kate"));
+        System.out.println(phoneBook.get("Bob"));
+        System.out.println(phoneBook.get("Arni"));
+        System.out.println(phoneBook.get("Lucy"));
+
+
+
+
+
+
+    }
 }
