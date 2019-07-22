24afad33a7acb1088cb005b8f47381e6fceca4cd
Vitas1968 <dor1968@mail.ru>
2019-07-22 18:23:38 +0700

    реализация класса PhoneBook

--- src/Lesson3/PhoneBook.java
+++ src/Lesson3/PhoneBook.java
@@ -1,5 +1,32 @@
 package Lesson3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class PhoneBook
 {
+    HashMap<String, ArrayList<String>> phoneBook;
+    String name;
+    ArrayList<String> phoneNumbers;
+
+    public PhoneBook()
+    {
+        phoneBook= new HashMap <>();
+    }
+
+    void add(String name, String... phones)
+    {
+        phoneNumbers=new ArrayList <>();
+        this.name = name;
+        Collections.addAll(phoneNumbers,phones);
+        phoneBook.put(this.name,phoneNumbers);
+    }
+
+    ArrayList<String> get(String name)
+    {
+        return phoneBook.get(name);
+    }
+
+
 }
