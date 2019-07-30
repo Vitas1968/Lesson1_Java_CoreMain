d41fe73a52310607ee664f0ebe187130a9d8c1b8
Vitas1968 <dor1968@mail.ru>
2019-07-30 20:31:37 +0700

    добавил два комментария

--- src/Lesson5/MainTesThread.java
+++ src/Lesson5/MainTesThread.java
@@ -4,9 +4,12 @@ public class MainTesThread
 {
     public static void main(String[] args)
     {
+        // задаём число потоков
         int numberOfThreads = 4;
         Calculatior calc = new Calculatior(numberOfThreads);
+        // расчет линейеым способом
         calc.calcTrigArray();
+        // расчет многопоточным способом
         calc.mainMethod();
 
     }
